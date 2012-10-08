3f598a39261d56e8d4b8014d100eb9df320dfa07
dmudireddy@gmail.com <dmudireddy@gmail.com>
2012-10-08 00:55:28 -0700

    updated with SSL

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Leatherstore::Application.config.session_store :cookie_store, :key => '_leatherstore_session'
-
+Leatherstore::Application.config.session_store :cookie_store, :key => '_leatherstore_session',:secure => Rails.env.production?
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
