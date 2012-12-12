96f8cabc2f1fc30b2752463ebd142ff82d86aeed
dmudireddy@gmail.com <dmudireddy@gmail.com>
2012-12-12 11:22:47 -0800

    12th Dec

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,27 @@ class Cart < ActiveRecord::Base
  
  
   has_many :line_items, :dependent => :destroy
+  
+
+def paypal_url(return_url)
+  values = {
+    :business => 'seller_1354927476_biz@gmail.com',
+    :cmd => '_cart',
+    :upload => 1,
+    :return => return_url,
+    :invoice => id
+  }
+  line_items.each_with_index do |item, index|
+    values.merge!({
+      "amount_#{index+1}" => item.product.price,
+      "item_name_#{index+1}" => item.product.name,
+      "quantity_#{index+1}" => item.quantity
+    })
+  end
+  "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+end
+
+
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end
